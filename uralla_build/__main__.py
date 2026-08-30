0029d5f75b858350382e80355e86bd02d2854ac6
Alexey Klyanin <42924160+uralla@users.noreply.github.com>
2026-08-30 11:08:05 +0500

    Launch interactive UI with no arguments

--- uralla_build/__main__.py
+++ uralla_build/__main__.py
@@ -1,4 +1,12 @@
-from .entrypoint import main
+from __future__ import annotations
 
+import sys
+
+from .entrypoint import main
+from .interactive import run_interactive
+
+
+if len(sys.argv) == 1:
+    raise SystemExit(run_interactive())
 
 raise SystemExit(main())
